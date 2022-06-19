5e1b240153e84ce2edb47bd3324506664a9675bb
Pawel Nackiewicz <it.pnackiewicz@gmail.com>
2022-06-19 11:04:55 +0200

    add navigation

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Navigation } from './Navigation';
 
 interface PageProps {
   children: React.ReactNode;
@@ -7,7 +8,7 @@ interface PageProps {
 export const Page = ({ children }: PageProps) => {
   return (
     <div className="w-full h-full min-h-screen flex flex-col items-center antialiased bg-slate-100">
-      <nav>Nav</nav>
+      <Navigation />
       <div className="flex-grow flex items-center justify-center">{children}</div>
       <footer>Pawi 2022</footer>
     </div>
---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Page } from '../components/Page';
+
+const AboutPage = () => {
+  return (
+    <Page>
+      <h2>About Page</h2>
+    </Page>
+  );
+};
+
+export default AboutPage;
